6dfe401bf1fe8c2eaaa530e8dd093eb550cec8ab
Monte Lai <monte.lai@consensys.net>
2023-01-05 20:26:38 +0800

    update text in hold to reveal modal

--- ui/components/app/modals/hold-to-reveal-modal/hold-to-reveal-modal.js
+++ ui/components/app/modals/hold-to-reveal-modal/hold-to-reveal-modal.js
@@ -1,19 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Identicon from '../../../ui/identicon';
 import Button from '../../../ui/button';
 import withModalProps from '../../../../helpers/higher-order-components/with-modal-props';
-import Modal from '../modal';
-import Typography from '../../../ui/typography';
-import {
-  TYPOGRAPHY,
-  FONT_WEIGHT,
-  ALIGN_ITEMS,
-  BLOCK_SIZES,
-  DISPLAY,
-} from '../../../../helpers/constants/design-system';
+import { Text } from '../../../component-library';
+import { TEXT } from '../../../../helpers/constants/design-system';
 import HoldToRevealButton from '../../hold-to-reveal-button';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
+import ZENDESK_URLS from '../../../../helpers/constants/zendesk-url';
 
 const HoldToRevealModal = ({ onLongPressed, hideModal }) => {
   const t = useI18nContext();
@@ -23,19 +16,56 @@ const HoldToRevealModal = ({ onLongPressed, hideModal }) => {
     hideModal();
   };
 
+  const handleCancel = () => {
+    hideModal();
+  };
+
   return (
-    <div>
-      <Typography variant={TYPOGRAPHY.H2} fontWeight={FONT_WEIGHT.NORMAL}>
-        Keep your SRP safe
-      </Typography>
-      <Typography variant={TYPOGRAPHY.H9} fontWeight={FONT_WEIGHT.NORMAL}>
-        Your Secret Recovery Phrase provides full access to your wallet and
-        funds
-      </Typography>
-      <HoldToRevealButton
-        buttonText="Hold to reveal SRP"
-        onLongPressed={unlock}
-      />
+    <div className="hold-to-reveal-modal">
+      <div className="hold-to-reveal-modal__header">
+        <div className="hold-to-reveal-modal__title">
+          <Text variant={TEXT.HEADING_SM}>{t('holdToRevealTitle')}</Text>
+        </div>
+        <button
+          className="hold-to-reveal-modal__close"
+          onClick={handleCancel}
+        />
+        
+      </div>
+      <div className="hold-to-reveal-modal__content">
+        <Text variant={TEXT.BODY_MD}>
+          {t('holdToRevealContent1', [
+            <Text key="hold-to-reveal-2" variant={TEXT.BODY_MD_BOLD} as="span">
+              {t('holdToRevealContent2')}
+            </Text>,
+          ])}
+        </Text>
+        <br />
+        <Text variant={TEXT.BODY_MD_BOLD}>
+          {t('holdToRevealContent3', [
+            <Text key="hold-to-reveal-4" variant={TEXT.BODY_MD} as="span">
+              {t('holdToRevealContent4')}
+            </Text>,
+
+            <Button
+              key="hold-to-reveal-5"
+              type="link"
+              href={ZENDESK_URLS.NON_CUSTODIAL_WALLET}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="settings-page__inline-link"
+            >
+              {t('holdToRevealContent5')}
+            </Button>,
+          ])}
+        </Text>
+      </div>
+      <div className="hold-to-reveal-modal__footer">
+        <HoldToRevealButton
+          buttonText={t('holdToReveal')}
+          onLongPressed={unlock}
+        />
+      </div>
     </div>
   );
 };
